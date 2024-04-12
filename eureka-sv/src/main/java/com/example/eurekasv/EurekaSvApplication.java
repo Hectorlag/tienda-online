8ecdcfa9642743d7fd05ad897f431aef0a76c77a
Hectorlag <hectorvlaguna@gmail.com>
2024-04-12 06:42:53 -0300

    Añadiendo archivos

---
+++ eureka-sv/src/main/java/com/example/eurekasv/EurekaSvApplication.java
@@ -0,0 +1,15 @@
+package com.example.eurekasv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSvApplication.class, args);
+	}
+
+}
---
+++ eureka-sv/src/test/java/com/example/eurekasv/EurekaSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekasv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
